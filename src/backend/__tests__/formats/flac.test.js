aa58a32ed664ff43b2a5a47d2d65dc882ed8bb75
lxndr <lxndr87i@gmail.com>
2018-07-15 17:32:25 +0300

    fixed tests

--- src/backend/__tests__/formats/flac.test.js
+++ src/backend/__tests__/formats/flac.test.js
@@ -3,14 +3,14 @@ import flacParser from '../../formats/flac';
 
 describe('FLAC parser', () => {
   it('opens file without tags', async () => {
-    const file = path.join(__dirname, '../fixtures/audio-01.flac');
-    const data = await flacParser({ file });
+    const filename = path.join(__dirname, '../fixtures/audio-01.flac');
+    const data = await flacParser({ filename });
     expect(data).toMatchSnapshot();
   });
 
   it('opens file with tags', async () => {
-    const file = path.join(__dirname, '../fixtures/audio-02.flac');
-    const data = await flacParser({ file });
+    const filename = path.join(__dirname, '../fixtures/audio-02.flac');
+    const data = await flacParser({ filename });
     expect(data).toMatchSnapshot();
   });
 });
